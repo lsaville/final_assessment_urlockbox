c9f55f65ca20fab1223175cb2cf61fc30b6b9980
Lee Saville <l.w.saville@gmail.com>
2017-02-27 10:23:59 -0700

    Add validations to pass the validations test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, :password, presence: :true
+  validates :email, uniqueness: :true
+  validates_confirmation_of :password
 end
