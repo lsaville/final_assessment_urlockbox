52c0add54b74c17b10ab3f0d93cd873acad56c0d
Lee Saville <l.w.saville@gmail.com>
2017-02-27 11:42:29 -0700

    Add sad path testing for login

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 describe 'user logs in' do
+  let (:user) { User.create(email: 'o@bama.com', password: 'michelle')}
+
   context 'with valid credentials' do
     scenario 'they are redirected with a success message' do
-      user = User.create(email: 'o@bama.com', password: 'michelle')
       visit '/'
 
-      fill_in "email", with: 'o@bama.com'
+      fill_in "email", with: user.email
       fill_in 'password', with: 'michelle'
       click_on 'Submit'
 
@@ -14,6 +15,18 @@ describe 'user logs in' do
       expect(page).to have_content("Successfully logged in!")
       expect(page).to have_content("Logout")
       expect(page).to_not have_content("Login")
+    end
+  end
+
+  context 'with invalid credentials' do
+    scenario 'missing password they see the login again with an unsuccessful message' do
+      visit '/'
+
+      fill_in 'email', with: user.email
+      click_on 'Submit'
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Something went wrong!")
     end
   end
 end
