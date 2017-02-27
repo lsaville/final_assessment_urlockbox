388aa9f4429bbd692736eb7b7760ace89b03dce0
Lee Saville <l.w.saville@gmail.com>
2017-02-27 10:23:02 -0700

    Add testing for expected validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'should validate email presence' do
+      user = FactoryGirl.build(:user, email: nil)
+
+      expect(user).to_not be_valid
+    end
+
+    it 'should validate email uniqueness' do
+      user = FactoryGirl.create(:user, email: 'email')
+      user2 = FactoryGirl.build(:user, email: 'email')
+
+      expect(user2).to_not be_valid
+    end
+
+    it 'should validate password presence' do
+      user = FactoryGirl.build(:user, password: nil)
+
+      expect(user).to_not be_valid
+    end
+  end
 end
