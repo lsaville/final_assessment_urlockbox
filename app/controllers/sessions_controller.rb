cb33adae82b0ba60b1a1f3103a880666b905d6c1
Lee Saville <l.w.saville@gmail.com>
2017-02-27 11:34:09 -0700

    Add success flash amd redirect to links path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
+      flash[:success] = "Successfully logged in!"
       session[:user_id] = user.id
-      redirect_to :root
+      redirect_to links_path
     else
       redirect_to :login
     end
