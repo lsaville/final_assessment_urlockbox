4e0dc3c7f4ccb99fdb1064c73ce746bf05b0b9c3
Lee Saville <l.w.saville@gmail.com>
2017-02-27 11:33:22 -0700

    Add helper method authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :authorize
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -11,5 +11,11 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     current_user
+  end
+
+  def authorize
+    if !logged_in?
+      redirect_to login_path
+    end
   end
 end
