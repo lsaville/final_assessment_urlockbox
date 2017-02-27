1601eb5c1e6864a9fb99817a2e87768b771f347a
Lee Saville <l.w.saville@gmail.com>
2017-02-27 16:01:12 -0700

    Add test to verify that the link updates

--- spec/features/user_can_edit_a_link_spec.rb
+++ spec/features/user_can_edit_a_link_spec.rb
@@ -9,13 +9,34 @@ describe 'user edits link', js: true do
       link = bama.links.first
 
       visit '/'
-      # page.save_screenshot('~/Desktop/blah1.png')
 
       within('#links-list') do
         click_on "Edit"
       end
-# page.save_screenshot('~/Desktop/blah2.png')
+
       expect(current_path).to eq("/links/#{link.id}/edit")
+    end
+  end
+
+  context 'they edit the link and are redirected to the links path' do
+    scenario 'they change the title' do
+      bama = user
+      log_in(bama)
+      bama.links.create(title: 'cat vids', url: 'https://www.youtube.com/watch?v=tntOCGkgt98')
+      link = bama.links.first
+
+      visit "/links/#{link.id}/edit"
+
+      fill_in "Title:", with: "extreme cat vids"
+      click_on "Edit Link"
+
+      sleep 1
+
+      expect(current_path).to eq(links_path)
+      within('#links-list') do
+        expect(page).to have_text("extreme cat vids")
+        expect(page).to have_text("https://www.youtube.com/watch?v=tntOCGkgt98")
+      end
     end
   end
 end
