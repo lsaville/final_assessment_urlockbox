ff26d16da8cb7b3b85e060f6fc8d24bf7209284a
Lee Saville <l.w.saville@gmail.com>
2017-02-27 12:19:11 -0700

    Add to the existing create link spec Add sad path Add test of default to false

--- spec/features/create_link_spec.rb
+++ spec/features/create_link_spec.rb
@@ -1,19 +1,55 @@
 require "rails_helper"
 
 RSpec.describe "can create links", :js => :true do
-  scenario "Create a new link" do
+  let(:login) {
     user = User.create(email: 'o@bama.com', password: 'michelle')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    
-    visit "/"
-    fill_in "Title:", :with => "Turing"
-    fill_in "URL:", :with => "http://turing.io"
-    click_on "Add Link"
+  }
 
-    within('#links-list') do
-      expect(page).to have_text("Turing")
-      expect(page).to have_text("http://turing.io")
+  context "valid url" do
+    scenario "Create a new link" do
+      login
+      visit "/"
+      fill_in "Title:", :with => "Turing"
+      fill_in "URL:", :with => "http://turing.io"
+      click_on "Add Link"
+
+      within('#links-list') do
+        expect(page).to have_text("Turing")
+        expect(page).to have_text("http://turing.io")
+      end
     end
 
+    it 'is created with a default read value of false' do
+      login
+      visit "/"
+      fill_in "Title:", :with => "Turing"
+      fill_in "URL:", :with => "http://turing.io"
+      click_on "Add Link"
+
+      within('#links-list') do
+        expect(page).to have_text("Turing")
+        expect(page).to have_text("http://turing.io")
+        expect(page).to have_text("false")
+      end
+    end
+
+  end
+
+  context 'invalid url' do
+    scenario 'user sees an error message' do
+      login
+      visit "/"
+      fill_in "Title:", :with => "turing"
+      fill_in "URL:", :with => "turing.io"
+      click_on "Add Link"
+
+      expect(page).to have_content("Invalid url")
+
+      within('#links-list') do
+        expect(page).to_not have_text("Turing")
+        expect(page).to_not have_text("http://turing.io")
+      end
+    end
   end
 end
