9a8dbc3b65ec520c282bb0cf536b54ae7f0b0698
Lee Saville <l.w.saville@gmail.com>
2017-02-27 11:34:58 -0700

    Add login stub

--- spec/features/create_link_spec.rb
+++ spec/features/create_link_spec.rb
@@ -2,6 +2,9 @@ require "rails_helper"
 
 RSpec.describe "can create links", :js => :true do
   scenario "Create a new link" do
+    user = User.create(email: 'o@bama.com', password: 'michelle')
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+    
     visit "/"
     fill_in "Title:", :with => "Turing"
     fill_in "URL:", :with => "http://turing.io"
