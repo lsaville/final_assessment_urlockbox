bbdf4bc59bb89052e29691c07b5bf419d5b23344
Lee Saville <l.w.saville@gmail.com>
2017-02-27 17:20:24 -0700

    Pull the functions out of the document ready and add a second ajax call to hotreads to submit the link

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -1,15 +1,35 @@
 var $newLinkTitle, $newLinkUrl;
 
 $(document).ready(function(){
-
-  $('#links-list').on('click', 'button.mark-read', function(){
-    var $this = $(this);
-    var linkId = $this.parents('.link').data('id');
-    
-    $.ajax({
-      url: '/api/v1/links/' + linkId,
-      method: 'PATCH',
-      data: {read: true}
-    });
-  })
+  $('#links-list').on('click', 'button.mark-read', markAsRead)
 })
+
+function markAsRead(e) {
+  e.preventDefault();
+
+  var $this = $(this);
+  var linkId = $this.parents('.link').data('id');
+  var linkUrl = $this.parents('.link').data('url');
+
+  $.ajax({
+    url: '/api/v1/links/' + linkId,
+    method: 'PATCH',
+    data: {read: true}
+  })
+   .then(updateLinkStatus)
+   .fail(displayFailure);
+
+  $.ajax({
+    type: "POST",
+    url: 'http://localhost:8080/api/v1/reads',
+    data: {read: linkUrl},
+  })
+}
+
+function updateLinkStatus(link) {
+  $(`.link[data-link-id=${link.id}]`).find(".read-status").text(link.read);
+}
+
+function displayFailure(failureData){
+  console.log("FAILED attempt to update Link: " + failureData.responseText);
+}
